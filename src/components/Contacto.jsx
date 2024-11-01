72196cbd1a78bf3996c0f1bff7d5d05a93968173
ccmillan <148159540+ccmillan@users.noreply.github.com>
2024-11-01 17:47:27 -0400

    nivel 1 de formulario
    nivel basico

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-router-dom";
 
 const Contacto = () => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("enviado");
+    console.log("enviado", { nombre, email });
+  };
+
+  const handleNombre = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
   };
   return (
     <div className="container">
       <h1 className="main-title">Contacto</h1>
       <form className="formulario" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Ingresa tu nombre" />
-        <input type="text" placeholder="Ingresa tu e-mail" />
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          value={nombre}
+          onChange={handleNombre}
+        />
+        <input
+          type="text"
+          placeholder="Ingresa tu e-mail"
+          value={email}
+          onChange={handleEmail}
+        />
         <button type="submit" className="enviar">
           Enviar
         </button>
